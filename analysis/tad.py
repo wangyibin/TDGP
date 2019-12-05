332a6748e77105d37e96f4bd429adbcff44463e4
Yibin Wang <738339463@qq.com>
2019-12-05 14:26:55 +0800

    vscode start

--- analysis/tad.py
+++ analysis/tad.py
@@ -18,9 +18,10 @@ import sys
 from collections import OrderedDict, defaultdict
 from intervaltree import Interval, IntervalTree
 from optparse import OptionParser
-from TDGP.apps.base import ActionDispatcher
-from TDGP.apps.base import debug, listify
-from TDGP.apps.base import BaseFile, Line
+from apps.base import ActionDispatcher
+from apps.base import debug, listify
+from apps.base import BaseFile, Line
+
 
 
 debug()
@@ -30,7 +31,8 @@ def main():
     actions = (
         ('plotSizeDist', 'plot the tad size distribution a list of samples'),
         ('getBottom', 'get bottom tads from hitad results'),
-        ('stat', 'stat TADs informations')
+        ('stat', 'stat TADs informations'),
+        ('getBoundaryBed', 'get tad boundary'),
     )
     p = ActionDispatcher(actions)
     p.dispatch(globals())
@@ -200,7 +202,8 @@ class TADFile(BaseFile):
 
 
     @classmethod
-    def getBoundaryBed(boundaryDict, chromSize, updistance=2e4, downdistance=2e4):
+    def getBoundaryBed(self, boundaryDict, chromSize, 
+            updistance=2e4, downdistance=2e4):
         """
         Get the TADs boundary bed with .
 
@@ -319,6 +322,43 @@ def getBottom(args):
     tf = TADFile(tad)
     for bottom in tf.bottom:
         print("\t".join(map(str, bottom)))
+
+
+def getBoundaryBed(args):
+    """
+    %prog <tad.bed> <chrom.sizes> [Options]
+
+    get a bed file of the tad boundary.
+    """
+    p = OptionParser(getBoundaryBed.__doc__)
+    p.add_option('-a', '--up', type=int, default=250,
+            help='the upstrean distance of boundary '
+            '[default: %default]')
+    p.add_option('-b', '--down', type=int, default=250,
+            help='the downstream distance of boundary '
+            '[default: %default]')
+    
+    opts, args = p.parse_args(args)
+    if len(args) != 2:
+        sys.exit(p.print_help())
+
+    tadFile, chromSize = args
+    up, down = opts.up, opts.down
+    if not op.exists(tadFile) or not \
+            op.exists(chromSize):
+        logging.error('The input file is not exists')
+    chrom_dict = dict(i.strip().split() \
+            for i in open(chromSize) if i.strip())
+
+    tf = TADFile(tadFile)
+    tf.getBoundaryDict()
+    boundaryBed = tf.getBoundaryBed(tf.boundaryDict, chrom_dict, 
+            up, down)
+    for item in sorted(boundaryBed):
+        print("\t".join(map(str, item[:3])))
+
+
+
 
 
 def plotSizeDist(args):
