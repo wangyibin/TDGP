e8a3292c449a8ec985f6c60732669dac0b99eb8c
wangyibin <738339463@qq.com>
2020-11-18 14:30:30 +0800

    TDGP init logging and warnings

--- __init__.py
+++ __init__.py
@@ -1,7 +1,16 @@
 import logging
+import warnings
 #logging.basicConfig(level=logging.INFO)
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
 logging.getLogger('numpy').setLevel(logging.WARNING)
+logging.getLogger('numexpr').setLevel(logging.WARNING)
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+
+warnings.simplefilter(action="ignore", category=RuntimeWarning)
+warnings.simplefilter(action="ignore", category=PendingDeprecationWarning)
+
 
 __author__ = ("Yibin Wang")
 __copyright__ = "Copyright (c) 2019, Yibin Wang"
